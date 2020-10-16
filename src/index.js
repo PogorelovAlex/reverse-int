b477a896af6d311373e2f55935a26bf66d1af8e7
Alexei Pogorelov <alexeipogorelovbn@yandex.ru>
2020-10-16 12:27:46 +0300

    feat:add revers function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    
+    let positiveN;
+	if (n<0){positiveN=-1*n;}else{positiveN=n};
+    
+    let arr = positiveN.toString().split('');
+    let result;
+
+    let middleIndex = Math.ceil((arr.length-1)/2);
+    let firstPart=arr.slice(0,middleIndex+1).reverse();
+    let secondPart=arr.slice(middleIndex+1);
+
+    return result =[...secondPart,...firstPart].join("");
 }
